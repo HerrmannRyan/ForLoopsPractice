31eaa0a9891e0fed126e29c8bae00ed39e2ac738
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2019-08-30 09:56:58 -0600

    First Commit

---
+++ For Loop Practice/src/ForLoopPractice.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class ForLoopPractice
+	{
+
+		static Scanner userInput = new Scanner (System.in);
+		
+		public static void main(String[] args)
+			{
+				//printDonuts();
+				//ascendAndDescend();
+				//printMultiplesOfFive();
+				//totalNumbers();
+				sumNumbersInUsersRange();
+
+			}
+
+		public static void printDonuts()
+			{
+				for(int i = 1; i <= 5; i++)
+				{
+					System.out.println("I love Donuts!!");	
+				}
+				
+			}
+
+		public static void ascendAndDescend()
+			{
+				for(int i = 1; i <= 5; i ++)
+				{
+					System.out.println(i);
+
+				}
+				for(int i = 5; i >= 0; i--)
+				{
+					System.out.println(i);	
+				}
+				
+			}
+
+		public static void printMultiplesOfFive()
+			{
+				
+				for(int i = 5; i <= 100; i+=5)
+				{
+					System.out.println(i);	
+				}
+				
+			}
+
+		public static void totalNumbers()
+			{
+				int sum = 0;
+				for(int i = 5; i <= 100; i+=5)
+				{
+					sum += i;	
+				}
+				System.out.println("The sum is equal to " + sum);
+			}
+
+		public static void sumNumbersInUsersRange()
+			{
+				
+			 System.out.println("Please puut in the lower bound.");
+			 int lowerBound = userInput.nextInt();
+			 System.out.println("Please put in the upper bound.");
+			 int upperBound = userInput.nextInt();
+			 
+			 for(int i = lowerBound; i <= upperBound; i++)
+			 {
+				 int sum = i++;
+			 }
+			
+			}
+
+	}
